87e093da71f59cdafff717cb5ebb849652ca5a4f
Ruben <rubenalvarado070@gmail.com>
2024-06-03 23:29:45 -0500

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,8 @@
     <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
       
       <div class="text-center">
-        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Crea anuncios para ecommerces en minutos!</h1>
-        <p class="mt-6 text-lg leading-8 text-gray-600">La parte más importante de un anuncio es el gancho y el guión!</p>
+        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl dark:text-white">Crea anuncios para ecommerces en minutos!</h1>
+        <p class="mt-6 text-lg leading-8 text-gray-600 dark:text-slate-400">La parte más importante de un anuncio es el gancho y el guión!</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
           <a href="/signup" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Empieza gratis!</a>
         </div>
